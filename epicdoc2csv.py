43d2bc363589bea2fe89e69da1f9c61d014c78a5
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2023-08-07 12:55:48 +1000

    flattened csv
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

--- epicdoc2csv.py
+++ epicdoc2csv.py
@@ -9,9 +9,12 @@ import epidoc
 import shutil
 import os
 import tqdm
+import json
+import numpy as np
+import pandas as pd
 
 SOURCE_DIR = "ISicily-ISicily-cf52385/inscriptions"
-TARGET_DIR = "output_csv"
+TARGET_DIR = "output"
 
 # clean target dir by deleting and recreating
 shutil.rmtree(TARGET_DIR, ignore_errors=True)
@@ -20,10 +23,12 @@ os.mkdir(TARGET_DIR)
 docs = []
 
 # get all files in source dir
-for file in tqdm.tqdm(glob.glob(f"{SOURCE_DIR}/*.xml")):
+for i, file in enumerate(tqdm.tqdm(glob.glob(f"{SOURCE_DIR}/*.xml"))):
     with open(file) as f:
         doc = epidoc.load(f)
     docs.append(doc)
+    # if i > 10:
+    #     break
     # break
 
 output_docs = []
@@ -32,19 +37,39 @@ output_docs = []
 # Get Title, All instances of idno and terms
 # Terms hide under textClass
 
+max_terms = 0
+
 for doc in tqdm.tqdm(docs):
     line = {}
     line["title"] = doc.title
     line["idno"] = doc.idno
-    line["terms"] = doc.terms
+    for i, term in enumerate(doc.terms):
+        # prepend _{i} to each key inside term
+        for key in term.copy().keys():
+            term[f"term_{key}_{i}"] = term.pop(key) 
+        line[f"term_{i}"] = term
+        if max_terms < i:
+            max_terms = i
+
     output_docs.append(line)
 
-# Write to CSV
-with open(f"{TARGET_DIR}/output.csv", "w") as f:
-    writer = csv.DictWriter(f, fieldnames=["title", "idno", "terms"])
-    writer.writeheader()
-    for doc in output_docs:
-        writer.writerow(doc)
 
+
+# Write to JSON
+with open(f"{TARGET_DIR}/output.json", "w") as f:
+    json.dump(output_docs, f, indent=4)
+
+
+
+# Make output_docs a dataframe
+df = pd.DataFrame(output_docs)
+# flatten the idno dictionary into the dataframe
+df = pd.concat([df.drop(["idno"], axis=1), df["idno"].apply(pd.Series)], axis=1)
+for i in range(max_terms+1):
+    print(i)
+    df = pd.concat([df.drop([f"term_{i}"], axis=1), df[f"term_{i}"].apply(pd.Series)], axis=1)
+
+# Write to CSV
+df.to_csv(f"{TARGET_DIR}/output.csv", index=False)
 
 
